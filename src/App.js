e11767d46511fcfc6c4736507ee851308fe1b7bf
DESKTOP-2UB1LAG\Saurin Shah <Saurin.Shah@softwebsolutions.com>
2025-06-02 17:07:02 +0530

    Iniaitial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
 
 function App() {
   return (
@@ -8,14 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. HEY! THIS IS A CHANGE I MADE.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
